582b289bf821a212f4253442feddb4535976035a
Ali GEHA <geha.ali@gmail.com>
2018-02-19 14:49:29 +0100

    feat exo-03

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -2,8 +2,39 @@ import React from 'react';
 
 import Person from './Person';
 
-const Discover = ({ people }) => (
-  <Person person={people[Math.floor(Math.random() * people.length)]} />
+const cycleOrder = (state, props) => (
+  { order: (state.order+1) % props.people.length }
 );
+
+
+class Discover extends React.Component {  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      order: 0,      
+    };
+  }
+
+  componentDidMount() {
+    this.iid = setInterval(() => this.showNextPerson(), 2000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.iid);
+  }
+
+  showNextPerson() {
+    this.setState(cycleOrder);
+  }
+
+  render() {
+    const { people } = this.props;
+    const { order } = this.state;
+    return (
+      <Person person={people[order]} />
+    );    
+  }
+}
 
 export default Discover;
